30d7fc81dde883643d1f30444cdd503cdab8646f
Abdullah Alsalem <abdullahalsalem18@gmail.com>
2021-09-25 02:00:43 +0300

    حل تحديات الجزء السابع

--- JS/Part_7.js
+++ JS/Part_7.js
@@ -1,6 +1,37 @@
 // تمرير قيمة الأس
 
 function exponent_x(number, exponent) {
-    // write your code here
-    return Math.pow(number, exponent);    
-  }
+  // write your code here
+  return Math.pow(number, exponent);
+}
+
+// إزالة الرموز من النصوص - السامر القحطاني
+function removeSpecialCharacters(str) {
+  // write your code here
+  return str.match(/(\w+|\-+|\s)/g).join('');
+}
+
+// أقصى عدد على اليسار - السامر القحطاني
+function leftDigit(str) {
+  // write your code here
+  return str.match(/\d/).join('');
+}
+
+// التحقق من وجود مسافة في الجملة - السامر القحطاني
+function hasSpace(str) {
+	// write your code here
+	return str.replace(/\s/g, "#")
+}
+
+// تعديل صيغة التاريخ - السامر القحطاني
+function dateFormating(date) {
+	// write your code here
+	return date.split('-').reverse().join('-');
+}
+
+// طباعة سلسلة من الأرقام
+
+function numbers_range(number) {
+  // write your code here
+  return Array(number + 1).fill(0).map((_, i) => i * 1).join(' ');
+}
