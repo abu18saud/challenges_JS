07dc3ac20b3f0e89066a87dc344ea1e46f8be719
Abdullah Alsalem <441226478@tvtc.edu.sa>
2022-08-19 23:09:47 +0300

    تكرار النص

--- JS/Part_2.js
+++ JS/Part_2.js
@@ -2,12 +2,55 @@
  * قم بكتابة function تستقبل مصفوفة array مكونة من أعداد صحيحة integer ثم قم بإرجاع المتوسط الحسابي لهذه الأعداد المرسلة في المصفوفة.
  */
 
- function average(array) {
-    sum = array.reduce((a, b) => a + b, 0);
-    len = array.length;
-    return sum/len;
+//للمبتدئين
+function average(array) {
+    sum = 0;
+
+    for (i = 0; i < array.lenegth; i++) {
+        sum += array[i];
+    }
+
+    return sum / array.lenegth;
 }
 
-/**
- * 
+//للمتقدمين
+function average(array) {
+    sum = array.reduce((a, b) => a + b, 0);
+    return sum / array.length;
+}
+
+/** الجمع التراكمي
+ * قم بكتابة دالة function تستقبل مصفوفة array من نوع integer وتقوم بإرجاع حاصل جمع جميع الأعداد و عدد العناصر في الـ مصفوفة array.
  */
+function cumulative_addition(elements_array) {
+    // write your code here
+    sum = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    return [sum, array.length];
+}
+
+/** آخر عنصر في المصفوفة
+ * قم بكتابة دالة function تستقبل مصفوفة array من نوع integer. تقوم الدالة بإرجاع قيمة آخر عنصر في المصفوفة.
+ */
+function last_elm(arr) {
+    // write your code here
+    return arr[arr.length - 1];
+    //للمتقدمين
+    //return arr.at(-1);
+}
+
+/** تكرار النص
+ * قم بكتابة دالة function تستقبل قيمتين، قيمة نصية string وعدد صحيح موجب integer. تقوم الدالة بتكرار النص المدخل بحسب العدد الذي تم تمريره.
+ */
+function word_repeat(word, n) {
+    // write your code here
+    container = "";
+    for (i = 0; i < n; i++) {
+        container += word + " ";
+    }
+
+    return container;
+}
